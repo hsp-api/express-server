baeed79e8c2bfb47be4f15423053b7ec4d55aa28
hsp-api <63402593+hsp-api@users.noreply.github.com>
2020-04-13 13:18:46 +0530

    Add files via upload

--- getXMLStyleSheetController.js
+++ getXMLStyleSheetController.js
@@ -12,7 +12,7 @@ GetXMLStyleSheetController.prototype.getXMLStyleSheet = function (request, respo
     axios.post(`https://ghs-hsp-dotnet-w-service01.azurewebsites.net/common/api/Common/GetXMLStyleSheet`, request.body)
         .then(res => {
             var htmlResponse = res.data.htmlResult;
-            htmlResponse = htmlResponse.replace(/\\n\t/g, '');
+         htmlResponse = htmlResponse.toString().replace(/[^\x20-\x7E]/gmi, "");
             var $ = cheerio.load(htmlResponse);
             $('base').attr("href", './styleSheets');
             response.json({ data: $.html(), ok: true });
